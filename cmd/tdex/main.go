b6143dc322d8fdf0f9fd56e4c2b707d5da0cfcd6
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2020-11-10 11:35:31 +0100

    merge master

--- cmd/tdex/main.go
+++ cmd/tdex/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
 
+	"github.com/btcsuite/btcutil"
+	"github.com/gogo/protobuf/jsonpb"
+	"github.com/gogo/protobuf/proto"
 	"github.com/urfave/cli/v2"
 	"github.com/vulpemventures/go-elements/network"
 	"google.golang.org/grpc"
@@ -35,6 +41,9 @@ var (
 	// maxMsgRecvSize is the largest message our client will receive. We
 	// set this to 200MiB atm.
 	maxMsgRecvSize = grpc.MaxCallRecvMsgSize(1 * 1024 * 1024 * 200)
+
+	tdexDataDir = btcutil.AppDataDir("tdex-operator", false)
+	statePath   = path.Join(tdexDataDir, "state.json")
 )
 
 func main() {
@@ -54,12 +63,96 @@ func main() {
 		&unlockwallet,
 		&depositfee,
 		&depositmarket,
+		&market,
+		&listmarket,
+		&listswaps,
+		&openmarket,
+		&closemarket,
+		&updatestrategy,
+		&updateprice,
 	)
 
 	err := app.Run(os.Args)
 	if err != nil {
 		fatal(err)
 	}
+}
+
+func getState() (map[string]string, error) {
+	data := map[string]string{}
+
+	file, err := ioutil.ReadFile(statePath)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(file, &data)
+
+	return data, nil
+}
+
+func setState(data map[string]string) error {
+
+	if _, err := os.Stat(tdexDataDir); os.IsNotExist(err) {
+		os.Mkdir(tdexDataDir, os.ModeDir|0755)
+	}
+
+	file, err := os.OpenFile(statePath, os.O_RDONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	err = file.Close()
+	if err != nil {
+		return err
+	}
+
+	currentData, err := getState()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	mergedData := merge(currentData, data)
+
+	jsonString, err := json.Marshal(mergedData)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(statePath, jsonString, 0755)
+	if err != nil {
+		return fmt.Errorf("writing to file: %w", err)
+	}
+
+	return nil
+}
+
+func merge(maps ...map[string]string) map[string]string {
+	merge := make(map[string]string, 0)
+	for _, m := range maps {
+		for k, v := range m {
+			merge[k] = v
+		}
+	}
+	return merge
+}
+
+/*
+Modified from https://github.com/lightninglabs/pool/blob/master/cmd/pool/main.go
+Original Copyright 2017 Oliver Gugger. All Rights Reserved.
+*/
+func printRespJSON(resp interface{}) {
+	jsonMarshaler := &jsonpb.Marshaler{
+		EmitDefaults: true,
+		OrigName:     true,
+		Indent:       "\t", // Matches indentation of printJSON.
+	}
+
+	jsonStr, err := jsonMarshaler.MarshalToString(resp.(proto.Message))
+	if err != nil {
+		fmt.Println("unable to decode response: ", err)
+		return
+	}
+
+	fmt.Println(jsonStr)
 }
 
 func getOperatorClient(ctx *cli.Context) (pboperator.OperatorClient, func(),
